a498dacf184c0d9898d993b9de0ddce40e36f60f
Annika Hammervoll <annika_limfe@hotmail.com>
2018-07-02 18:14:03 +0200

    Adding tests

---
+++ tests/dnat-snat-firewall-2/nping.sh
@@ -0,0 +1,31 @@
+# Connect to apache server running on client4 (TCP)
+# Basically: nping --tcp-connect -p 1600 10.100.0.30
+# Should be dnat'ed to 10.100.0.160 port 8080
+
+# Prerequisite:
+# Run 'docker run -dit --name my-apache-app -p 8080:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd:2.4' on
+# lotto-client4 (10.100.0.160)
+
+sent=1000
+rate=200 # Requests pr second, higher than 5 requires sudo
+mode="--tcp-connect"
+port="1600"
+# delay=
+
+cmdOut=$(nping -c $sent $mode -p $port --rate $rate 10.100.0.30)
+
+res=$(printf "%s" "$cmdOut" | grep "Successful connections:")
+
+# attempts=$(printf "%s" "$res" | cut -d ' ' -f 4)
+received=$(printf "%s" "$res" | cut -d ' ' -f 8)
+# successful=$(printf "%s" "$res" | cut -d ' ' -f 8)
+# failed=$(printf "%s" "$res" | cut -d ' ' -f 11)
+
+jq  --arg dataSent $sent \
+    --arg dataReceived $received \
+    --arg dataRate $rate \
+    --arg dataFull "$cmdOut" \
+    '. | .["sent"]=($dataSent|tonumber) |
+    .["received"]=($dataReceived|tonumber) |
+    .["rate"]=($dataRate|tonumber) |
+    .["raw"]=$dataFull'<<<'{}'
---
+++ tests/dnat-snat-get-file/hey-get-file-test.sh
@@ -0,0 +1,35 @@
+# hey http testing
+# Script used for performing http requests (get file.txt from lotto-client4 10.100.0.160)
+# Returns rate and average response time
+
+# Prerequisites to test:
+# On lotto-client4 (10.100.0.160):
+# - Produce a 1G text file with random content:
+# 	base64 /dev/urandom | head -c 1G > file.txt
+# - Start apache server on 8080 (returns content of home folder):
+# 	docker run -dit --name my-apache-app -p 8080:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd:2.4
+
+# Input values to cmd
+sent=10000
+concurrency=200
+
+cmdOut=$(docker run --rm rcmorano/docker-hey -n $sent -c $concurrency http://10.100.0.30:1600/file.txt)
+
+# Parse output, important to set a default value if the command over fails
+received=$(printf "%s" "$cmdOut" | awk '/responses/ {print $2}' )
+if [ -z $received ]; then received=0; fi
+rate=$(printf "%s" "$cmdOut" | awk '/Requests\/sec/ {print $2}' )
+if [ -z $rate ]; then rate=0; fi
+avg=$(printf "%s" "$cmdOut" | awk '/Average/ {print $2}' )
+if [ -z $avg ]; then avg=0; fi
+
+jq  --arg dataSent $sent \
+	--arg dataReceived $received \
+	--arg dataRate $rate \
+	--arg dataAvg $avg \
+	--arg dataFull "$cmdOut" \
+	'. | .["sent"]=($dataSent|tonumber) |
+	.["received"]=($dataReceived|tonumber) |
+	.["rate"]=($dataRate|tonumber) |
+	.["avg"]=($dataAvg|tonumber) |
+	.["raw"]=$dataFull'<<<'{}'
