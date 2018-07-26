002ddf0fc23e4cbbd1b5da2433f6c817ef8dc284
Annika Hammervoll <annika_limfe@hotmail.com>
2018-07-26 21:08:34 +0200

    alias-update test: Avoiding unnecessary deployment and expanding the test to change the alias 50 times by default and verifying that the alias was actually changed each time

--- tests/alias-update/alias.sh
+++ tests/alias-update/alias.sh
@@ -3,21 +3,39 @@
 set -e
 
 moth="{{.MothershipBinPathAndName}}"
-original_alias={{.OriginalAlias}}
-ID=$($moth inspect-instance $original_alias -o id)
-new_alias=$(echo alias-test-"$(date | shasum | cut -d " " -f 1)")
+instAlias={{.OriginalAlias}}
+ID=$($moth inspect-instance $instAlias -o id)
+newAlias=$(echo alias-test-"$(date | shasum | cut -d " " -f 1)")
 
-# Change alias to new_alias
-cmdOut=$($moth instance-alias $ID $new_alias)
+alias=""
+sent=0
+received=0
 
-sleep 0.2
+# Update alias 50 times (alternate between instAlias and newAlias)
+for i in {1..50}
+do
+    # Set alias to use
+    if (( $i % 2 )); then
+        alias=$instAlias
+    else
+        alias=$newAlias
+    fi
 
-# Change alias back to old alias
-cmdOut+=$($moth instance-alias $ID $original_alias)
+    sent=$[$sent + 1]
+    # Change alias
+    cmdOut=$($moth instance-alias $ID $alias)
+    # Verify that the alias was actually changed
+    existingAlias=$($moth inspect-instance $ID -o json | jq -r '.alias')
+    if [[ "$existingAlias" == "$alias" ]]; then
+        received=$[$received + 1]
+    fi
+    sleep 0.2
+done
+
+# Reset to original alias
+cmdOut+=$($moth instance-alias $ID $instAlias)
 
 # If none of the commands above failed it means that we were successful
-sent=1
-received=1
 rate=0.1
 avg=0
 
