8d6ec328e6e03e05dfcc9ccf9d322fcbf7804c8d
Annika Hammervoll <annika_limfe@hotmail.com>
2018-08-01 11:58:54 +0200

    acorn, demo and udp tests: Updating test scripts to set received to 0 if empty output and UDP test should now be working

--- tests/udp/udp_nping.sh
+++ tests/udp/udp_nping.sh
@@ -8,11 +8,21 @@ port=4242
 data="hi"
 
 cmdOut=$(nping -c $sent --rate $rate $mode -p $port --data-string $data 10.100.0.30)
-res=$(printf "%s" "$cmdOut" | grep "Successful connections:")
+res=$(printf "%s" "$cmdOut" | grep "UDP packets")
+
+# Possible:
 # attempts=$(printf "%s" "$res" | cut -d ' ' -f 4)
+# if [ -z $attempts ]; then attempts=0; fi
+
 received=$(printf "%s" "$res" | cut -d ' ' -f 7)
+if [ -z $received ]; then received=0; fi
+# Or:
 # successful=$(printf "%s" "$res" | cut -d ' ' -f 7)
+# if [ -z $successful ]; then successful=0; fi
+
+# Possible:
 # failed=$(printf "%s" "$res" | cut -d ' ' -f 10)
+# if [ -z $failed ]; then failed=0; fi
 
 jq  --arg dataSent $sent \
     --arg dataReceived $received \
