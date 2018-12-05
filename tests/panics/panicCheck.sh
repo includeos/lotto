5dcf824573437a4073612cb88d66015b2ea302e2
Martin Nordsletten <mnordsletten@gmail.com>
2018-12-05 14:03:37 +0100

    Tests: Updated the rest of the tests

--- tests/panics/panicCheck.sh
+++ tests/panics/panicCheck.sh
@@ -10,35 +10,45 @@ image_ID={{.ImageID}}
 sent=1
 received=0
 rate=1
-avg=0
 
 # Check number of panics from before
 panics_before=$($moth instance-panics $alias | shasum | cut -d " " -f 1)
 
 # Deploy image to instance
-cmdOut="Deploy: ""$($moth deploy --wait $alias $image_ID)"" "
+raw="Deploy: ""$($moth deploy --wait $alias $image_ID)"" "
 
 # Wait until the panic has been received
 for i in {1..15}; do
   sleep 1
   panics_now=$($moth instance-panics $alias | shasum | cut -d " " -f 1)
   if [ "$panics_now" != "$panics_before" ]; then
-    cmdOut+="Time taken to receive panic: $i seconds"
+    raw+="Time taken to receive panic: $i seconds"
     received=1
-    avg=$i
     break
   else
     :
   fi
 done
 
-jq  --arg dataSent $sent \
-    --arg dataReceived $received \
-    --arg dataRate $rate \
-    --arg dataAvg $avg \
-    --arg dataFull "$cmdOut" \
-    '. | .["sent"]=($dataSent|tonumber) |
-    .["received"]=($dataReceived|tonumber) |
-    .["rate"]=($dataRate|tonumber) |
-    .["avg"]=($dataAvg|tonumber) |
-    .["raw"]=$dataFull'<<<'{}'
+if [ "$sent" -eq "$received" ]; then
+  result=true
+fi
+
+if [ -z $result ]; then result=false; fi
+if [ -z $sent ]; then sent=0; fi
+if [ -z $received ]; then received=0; fi
+if [ -z $rate ]; then rate=0; fi
+if [ -z $raw ]; then raw=""; fi
+jq \
+  --argjson result $result \
+  --argjson sent $sent \
+  --argjson received $received \
+  --argjson rate $rate \
+  --arg raw "$raw" \
+  '. |
+  .["result"]=$result |
+  .["sent"]=$sent |
+  .["received"]=$received |
+  .["rate"]=$rate |
+  .["raw"]=$raw
+  '<<<'{}'
